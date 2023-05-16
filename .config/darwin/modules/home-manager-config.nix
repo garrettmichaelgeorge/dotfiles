ed816921392c89429b894cb61f6b179397c81fb5
Garrett George <garrett.michael.george@gmail.com>
2023-05-16 10:19:48 -0500

    chore(home-manager): enable verbose output

--- .config/darwin/modules/home-manager-config.nix
+++ .config/darwin/modules/home-manager-config.nix
@@ -1,9 +1,11 @@
 { home-manager, ... }:
 
+# https://nix-community.github.io/home-manager/nix-darwin-options.html
 {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+    verbose = true;
     users.garrett = import ../users/garrett;
   };
 }
