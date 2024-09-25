815bce4091a0906ef4135cd97015e7628e3f051d
Garrett George <garrett.michael.george@gmail.com>
2024-09-25 10:28:36 -0500

    chore(nix): add Python overlay

--- .config/nixpkgs/overlays.nix
+++ .config/nixpkgs/overlays.nix
@@ -1,7 +1,25 @@
 [
+  # https://github.com/nix-community/neovim-nightly-overlay
   (self: super: {
     inherit (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+      url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
     }));
+  })
+
+  # https://nixos.org/manual/nixpkgs/unstable/#installing-environments-globally-on-the-system
+  (self: super: {
+    myEnv = super.buildEnv {
+      name = "myPython";
+      paths = [
+        # A Python 3 interpreter with some packages
+        (self.python3.withPackages (
+          ps: with ps; [
+            pynvim
+          ]
+        ))
+
+        # Some other packages we'd like as part of this env
+      ];
+    };
   })
 ]
