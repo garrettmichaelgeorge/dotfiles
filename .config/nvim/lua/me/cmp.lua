ed4af19eb79629c12ea300ddda61af49a7df25e8
Garrett George <garrett.michael.george@gmail.com>
2024-09-25 10:28:36 -0500

    feat(nvim): enhance auto-completion

--- .config/nvim/lua/me/cmp.lua
+++ .config/nvim/lua/me/cmp.lua
@@ -33,9 +33,15 @@ cmp.setup({
   snippet = {
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body)
+      -- vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
-  mapping = {
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Your configuration here.
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
@@ -56,13 +62,13 @@ cmp.setup({
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        cmp.select_prev_item()
+        -- elseif luasnip.jumpable(-1) then
+        --   cmp.select_prev_item()
       else
         fallback()
       end
     end,
-  },
+  }),
   formatting = {
     -- show name of completion item kind and source
     -- requires https://github.com/onsails/lspkind-nvim
@@ -71,21 +77,25 @@ cmp.setup({
       vim_item.menu = ({
         buffer = "[Buffer]",
         latex_symbols = "[Latex]",
-        luasnip = "[LuaSnip]",
+        -- luasnip = "[LuaSnip]",
         nvim_lsp = "[LSP]",
         nvim_lua = "[Lua]",
         omni = "[Omni]",
         path = "[Path]",
         treesitter = "[TS]",
-        ultisnips = "[Ulti]",
+        -- ultisnips = "[Ulti]",
         vim_dadbod_completion = "[DB]",
         vsnip = "[VSnip]",
+        git = "[Git]",
+        ctags = "[Ctags]",
+        cmdline = "[Cmd]",
       })[entry.source.name]
+
       return vim_item
     end
   },
   sources = {
-    -- { name = 'buffer' },
+    { name = 'buffer' },
     { name = 'calc' },
     { name = 'nvim_lsp' },
     { name = 'nvim_lua' },
@@ -95,15 +105,15 @@ cmp.setup({
     { name = 'vim_dadbod_completion' },
     { name = 'vsnip' },
     { name = 'neorg' },
+    { name = 'ctags' },
+    { name = 'git' },
+    -- { name = "ultisnips" }
   },
-  experimental = {
-    native_menu = false
-  };
-  ...
 })
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline({}),
   sources = {
     { name = 'buffer' }
   }
@@ -111,9 +121,128 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline({}),
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
-      { name = 'cmdline' }
-    })
+    { name = 'cmdline' }
+  })
 })
+
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Git completion source
+local format = require("cmp_git.format")
+local sort = require("cmp_git.sort")
+
+require("cmp_git").setup({
+  -- defaults
+  filetypes = { "gitcommit", "octo" },
+  remotes = { "upstream", "origin" }, -- in order of most to least prioritized
+  enableRemoteUrlRewrites = false,    -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
+  git = {
+    commits = {
+      limit = 100,
+      sort_by = sort.git.commits,
+      format = format.git.commits,
+    },
+  },
+  github = {
+    hosts = {}, -- list of private instances of github
+    issues = {
+      fields = { "title", "number", "body", "updatedAt", "state" },
+      filter = "all", -- assigned, created, mentioned, subscribed, all, repos
+      limit = 100,
+      state = "open", -- open, closed, all
+      sort_by = sort.github.issues,
+      format = format.github.issues,
+    },
+    mentions = {
+      limit = 100,
+      sort_by = sort.github.mentions,
+      format = format.github.mentions,
+    },
+    pull_requests = {
+      fields = { "title", "number", "body", "updatedAt", "state" },
+      limit = 100,
+      state = "open", -- open, closed, merged, all
+      sort_by = sort.github.pull_requests,
+      format = format.github.pull_requests,
+    },
+  },
+  gitlab = {
+    hosts = {}, -- list of private instances of gitlab
+    issues = {
+      limit = 100,
+      state = "opened", -- opened, closed, all
+      sort_by = sort.gitlab.issues,
+      format = format.gitlab.issues,
+    },
+    mentions = {
+      limit = 100,
+      sort_by = sort.gitlab.mentions,
+      format = format.gitlab.mentions,
+    },
+    merge_requests = {
+      limit = 100,
+      state = "opened", -- opened, closed, locked, merged
+      sort_by = sort.gitlab.merge_requests,
+      format = format.gitlab.merge_requests,
+    },
+  },
+  trigger_actions = {
+    {
+      debug_name = "git_commits",
+      trigger_character = ":",
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.git:get_commits(callback, params, trigger_char)
+      end,
+    },
+    {
+      debug_name = "gitlab_issues",
+      trigger_character = "#",
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.gitlab:get_issues(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = "gitlab_mentions",
+      trigger_character = "@",
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.gitlab:get_mentions(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = "gitlab_mrs",
+      trigger_character = "!",
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.gitlab:get_merge_requests(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = "github_issues_and_pr",
+      trigger_character = "#",
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
+      end,
+    },
+    {
+      debug_name = "github_mentions",
+      trigger_character = "@",
+      action = function(sources, trigger_char, callback, params, git_info)
+        return sources.github:get_mentions(callback, git_info, trigger_char)
+      end,
+    },
+  },
+}
+)
+
+-- Ultisnips source
+-- https://github.com/quangnguyen30192/cmp-nvim-ultisnips#installation-and-recommended-mappings
+-- require("cmp_nvim_ultisnips").setup {}
